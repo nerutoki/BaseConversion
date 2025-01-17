e298eb898a93c4348257d768e033eeb623efc9c2
sdfsdf <asfsdfsfsf@sadfasdfsd>
2025-01-16 14:29:47 -1000

    Added functions to backend

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@
 
 ## Functions
 
-def decimal_from_conversion(number, divider):
+def conversion_from_decimal(number, divider):
     remainder = []
     dict_hexadecimal_letters = {10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"}
 
@@ -23,32 +23,59 @@ def decimal_from_conversion(number, divider):
 
 
 def decimal_to_binary_conversion(number):
-    return decimal_from_conversion(number,2)
+    return conversion_from_decimal(number,2)
 
 def decimal_to_hexadecimal_conversion(number):
-    return decimal_from_conversion(number, 16)
+    return conversion_from_decimal(number, 16)
   
+def decimal_to_octal_conversion(number):
+    return conversion_from_decimal(number, 8)
 
+
+def conversion_to_decimal(number, divider):
+    dict_hexadecimal_letters = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
+
+    counter = 0
+    sum = 0
+    current = 0
+    start = 0
+    first_two_chars = "0x"
+
+    if first_two_chars == str(number)[:2]:
+        number = number[2:]
+
+    for x in str(number)[::-1]:
+
+        if x != "x":
+            if x in dict_hexadecimal_letters:
+                x = dict_hexadecimal_letters[x]
+            
+            current = (int(x) * (divider **counter))
+            print(x, "*", divider, "to the", counter, "= ", current)
+            sum = sum + current
+
+            counter += 1 
+
+    return sum
 
 def hexadecimal_to_decimal_conversion(number):
-    print(0)
+    return conversion_to_decimal(number, 16)
+    
 def binary_to_decimal_conversion(number):
-    binary = [x for x in str(number)]
+    return conversion_to_decimal(number, 2)
 
-    # prefix and post fix
-    # remainder = []
-
-    # while number >= 1:
-    #     remainder.append(number % 2)
-    #     print(number, "/", 2, "=", number//2, "Remainder = ", remainder[len(remainder)-1])
-    #     number = number // 2
-
-    # return "".join(str(x) for x in remainder[::-1])
-
-
+def octal_to_decimal_conversion(number):
+    return conversion_to_decimal(number, 8)
  
-# In hexadecimal, the digits 0-9 are the same as decimal, while 10-15 are represented by the letters A-F. 
 
+#octal to hexa
+# octal to binary
+
+#binary to octal
+# binary to hexa
+
+## hexa to octal
+## hexa to binary
 ### hex decimal function
 
 ## binary decimal function
@@ -56,10 +83,11 @@ def binary_to_decimal_conversion(number):
 
 if __name__ == "__main__":
 
-    number_1 = 26
+    number_1 = 45
 
-    print(decimal_to_binary_conversion(number_1))
-    print(decimal_to_hexadecimal_conversion(number_1))
+    # print(decimal_to_binary_conversion(number_1))
+    # print(decimal_to_hexadecimal_conversion(number_1))
 
-    print(binary_to_decimal_conversion(11010))
+    print(hexadecimal_to_decimal_conversion("0x4B6"))
+    print(binary_to_decimal_conversion(110))
 
